42e872c5ca81334497c206f48c319dabf1da6329
Isaac Jefferson <isaacjefferson.t@gmail.com>
2019-12-23 01:38:35 +0530

    Update constants.js

--- src/main/webapp/js/constants.js
+++ src/main/webapp/js/constants.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-var hostURL = "http://localhost:8080/CentraForklifts/";
+var hostURL = "http://open-shift-centra-forklifts-git-cfproject.2886795280-80-simba07.environments.katacoda.com/";
 var userRoleKey = "userRole";
 var invoiceIdKey = "invoiceId";
 var userFirstNameKey = "ufirstName";
